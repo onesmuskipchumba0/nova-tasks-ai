cd71bfa3b5f8558214e59e2f7c15bf1578606d81
OnesmusBett <onesmuskipchumba5@gmail.com>
2025-03-19 19:44:03 +0300

    add openai config file

--- utils/openai.ts
+++ utils/openai.ts
@@ -1,5 +1,21 @@
 import OpenAI from "openai";
 
-export default function OpenAIModel(prompt: string){
-    
+export async function OpenAIModel(prompt: string){
+    const client  = new OpenAI({
+        baseURL:"https://models.inference.ai.azure.com",
+        apiKey: process.env.OPENAI_API_KEY,
+    })
+
+    const response = await client.chat.completions.create({
+        model: "gpt-4o",
+        messages:[
+            {role: "system", content:"You are a helpful assistant who generates JSON data"},
+            { role:"user", content: `${prompt}, please generate this in JSON format and not as markdown or with backticks` }
+        ],
+        temperature: 1, 
+        max_tokens: 4096,
+        top_p: 1
+    })
+
+    return response.choices[0].message.content
 }
